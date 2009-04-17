14b1769dc117ae9a8ae86008d961407427242527
Stephen Green <stephen.green@sun.com>
2009-04-17 10:03:00 -0400

    Unset classpath.

--- lib/jini/startReggie.sh
+++ lib/jini/startReggie.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 INSTALLDIR=`dirname $0`
+unset CLASSPATH
 java -DauraGroup=$AURAGROUP -DauraPolicy=$INSTALLDIR/jsk-all.policy -Djava.security.policy=$INSTALLDIR/jsk-all.policy -jar $INSTALLDIR/lib/start.jar $INSTALLDIR/start.config
