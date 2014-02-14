8c2aed7bc7166cada734ea7da70a8764877c64dd
Stephen Green <stephen.x.green@oracle.com>
2014-02-14 10:40:48 -0500

    Get rid of main.

--- src/com/sun/labs/util/service/sample/HelloServiceImpl.java
+++ src/com/sun/labs/util/service/sample/HelloServiceImpl.java
@@ -42,11 +42,4 @@ public class HelloServiceImpl extends ConfigurableServiceAdapter implements Hell
         return "HelloService";
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
 }
