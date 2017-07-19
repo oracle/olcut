c6e39efc47c277229d8a987d4178d8a48e9e03fe
Adam Pocock <adam.pocock@oracle.com>
2017-07-19 17:28:52 -0400

    Adding tests for saving of generic lists/sets/maps.

--- src/test/java/com/sun/labs/util/props/ConfigurablePropertyMapTest.java
+++ src/test/java/com/sun/labs/util/props/ConfigurablePropertyMapTest.java
@@ -1,8 +1,11 @@
 package com.sun.labs.util.props;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import static org.junit.Assert.*;
 
@@ -10,6 +13,14 @@ import static org.junit.Assert.*;
  *
  */
 public class ConfigurablePropertyMapTest {
+
+    public File f;
+
+    @Before
+    public void setUp() throws IOException {
+        f = File.createTempFile("config", ".xml");
+        f.deleteOnExit();
+    }
 
     @Test
     public void configurablePropMap() throws IOException {
@@ -22,5 +33,29 @@ public class ConfigurablePropertyMapTest {
 
         assertEquals(fm.map.get("second").name, "foo2");
         assertEquals(fm.map.get("second").value, 2);
+    }
+
+    @Test
+    public void saveAllWithInstantiationGeneric() throws IOException {
+        URL cu = getClass().getResource("configurablePropMap.xml");
+        ConfigurationManager cm1 = new ConfigurationManager(cu);
+        FooMapConfigurable s1 = (FooMapConfigurable) cm1.lookup("fooMap");
+        cm1.save(f, true);
+        assertEquals(3, cm1.getNumConfigured());
+        ConfigurationManager cm2 = new ConfigurationManager(f.toURI().toURL());
+        FooMapConfigurable s2 = (FooMapConfigurable) cm2.lookup("fooMap");
+        assertEquals(s1, s2);
+    }
+
+    @Test
+    public void saveAllWithNoInstantiationGeneric() throws IOException {
+        URL cu = getClass().getResource("configurablePropMap.xml");
+        ConfigurationManager cm1 = new ConfigurationManager(cu);
+        cm1.save(f, true);
+        assertEquals(0, cm1.getNumConfigured());
+        ConfigurationManager cm2 = new ConfigurationManager(f.toURI().toURL());
+        FooMapConfigurable s1 = (FooMapConfigurable) cm1.lookup("fooMap");
+        FooMapConfigurable s2 = (FooMapConfigurable) cm2.lookup("fooMap");
+        assertEquals(s1, s2);
     }
 }
