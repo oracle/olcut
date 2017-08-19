d41a6f287443244f6e4d26d0615c69056f45abec
Adam Pocock <craigacp@gmail.com>
2017-08-19 11:28:50 -0400

    Adding a test which uses all the currently possible field types in the new config system.

--- src/test/java/com/sun/labs/util/props/NamedComponent.java
+++ src/test/java/com/sun/labs/util/props/NamedComponent.java
@@ -12,4 +12,18 @@ public class NamedComponent implements Component {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NamedComponent that = (NamedComponent) o;
+
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
