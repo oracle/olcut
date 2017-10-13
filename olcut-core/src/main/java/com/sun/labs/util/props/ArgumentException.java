f79acae75b00d89cee86e8500a95e6bbebabd293
Adam Pocock <craigacp@gmail.com>
2017-10-13 18:34:55 -0400

    Adding the start of arguments processing to ConfigurationManager. At the moment all it does is validate the arguments don't conflict, and generates a usage string.

---
+++ olcut-core/src/main/java/com/sun/labs/util/props/ArgumentException.java
@@ -0,0 +1,113 @@
+/*
+ * 
+ * Copyright 1999-2004 Carnegie Mellon University.  
+ * Portions Copyright 2004 Sun Microsystems, Inc.  
+ * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
+ * All Rights Reserved.  Use is subject to license terms.
+ * 
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL 
+ * WARRANTIES.
+ *
+ */
+package com.sun.labs.util.props;
+
+/** Indicates that a problem occurred while parsing arguments. */
+public class ArgumentException extends RuntimeException {
+
+    private final String argumentName;
+    private final String otherArgumentName;
+    private final String msg;
+
+    /**
+     * Creates a new argument exception.
+     *
+     * @param argumentName The component this exception is related to.
+     * @param msg          a description of the problem.
+     */
+    public ArgumentException(String argumentName, String msg) {
+        this(null, argumentName, null, msg);
+    }
+
+    /**
+     * Creates a new argument exception.
+     *
+     * @param argumentName The argument this exception is related to.
+     * @param otherArgumentName The name of the conflicting argument.
+     * @param msg          a description of the problem.
+     */
+    public ArgumentException(String argumentName, String otherArgumentName, String msg) {
+        this(null, argumentName, otherArgumentName, msg);
+    }
+
+
+    /**
+     * Creates a new argument exception.
+     *
+     * @param cause        The cause of exception. (or <code>null</code> if unknown)
+     * @param msg          a description of the problem.
+     */
+    public ArgumentException(Throwable cause, String msg) {
+        this(cause,null,null,msg);
+    }
+
+    /**
+     * Creates a new argument exception.
+     *
+     * @param cause        The cause of the exception. (or <code>null</code> if unknown)
+     * @param argumentName The argument this exception is related to.  (or <code>null</code> if unknown)
+     * @param msg          a description of the problem.
+     */
+    public ArgumentException(Throwable cause, String argumentName, String msg) {
+        this(cause,argumentName,null,msg);
+    }
+
+    /**
+     * Creates a new argument exception.
+     *
+     * @param cause        The cause of the exception. (or <code>null</code> if unknown)
+     * @param argumentName The argument this exception is related to.
+     * @param otherArgumentName The name of the conflicting argument.
+     * @param msg          a description of the problem.
+     */
+    public ArgumentException(Throwable cause, String argumentName, String otherArgumentName, String msg) {
+        super(cause);
+
+        this.argumentName = argumentName;
+        this.otherArgumentName = otherArgumentName;
+        this.msg = msg;
+    }
+
+    /** @return Returns the msg. */
+    public String getMsg() {
+        return msg;
+    }
+
+    /**
+     * Retrieves the name of the offending argument
+     *
+     * @return the name of the offending argument
+     */
+    public String getArgument() {
+        return otherArgumentName;
+    }
+
+    /**
+     * Returns a string representation of this object
+     *
+     * @return the string representation of the object.
+     */
+    public String toString() {
+        if (argumentName != null) {
+            if (otherArgumentName != null) {
+                return "Argument Exception argument:'" + argumentName + "' other argument:'" + otherArgumentName + "' - " + msg + '\n'
+                        + super.toString();
+            } else {
+                return "Argument Exception argument:'" + argumentName + "' - " + msg + '\n' + super.toString();
+            }
+        } else {
+            return "Argument Exception - " + msg + '\n' + super.toString();
+        }
+
+    }
+}
