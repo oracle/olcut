8807ea262efb17af49d7248e0a098b700c243ccc
Adam Pocock <craigacp@gmail.com>
2017-10-13 18:36:04 -0400

    Removing incorrect copyright statement from ArgumentException.

--- olcut-core/src/main/java/com/sun/labs/util/props/ArgumentException.java
+++ olcut-core/src/main/java/com/sun/labs/util/props/ArgumentException.java
@@ -1,15 +1,3 @@
-/*
- * 
- * Copyright 1999-2004 Carnegie Mellon University.  
- * Portions Copyright 2004 Sun Microsystems, Inc.  
- * Portions Copyright 2004 Mitsubishi Electric Research Laboratories.
- * All Rights Reserved.  Use is subject to license terms.
- * 
- * See the file "license.terms" for information on usage and
- * redistribution of this file, and for a DISCLAIMER OF ALL 
- * WARRANTIES.
- *
- */
 package com.sun.labs.util.props;
 
 /** Indicates that a problem occurred while parsing arguments. */
