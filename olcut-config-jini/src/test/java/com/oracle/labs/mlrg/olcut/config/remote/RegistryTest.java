00e352f89e6c62c5b058371157e605e4378b82b2
Adam Pocock <adam.pocock@oracle.com>
2018-03-27 09:20:43 -0400

    Adding support for ConfigurationEntries to lookupAll in olcut-config-jini.

--- olcut-config-jini/src/test/java/com/oracle/labs/mlrg/olcut/config/remote/RegistryTest.java
+++ olcut-config-jini/src/test/java/com/oracle/labs/mlrg/olcut/config/remote/RegistryTest.java
@@ -9,6 +9,7 @@ import com.oracle.labs.mlrg.olcut.util.SimpleLabsLogFormatter;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.logging.Handler;
 import java.util.logging.Logger;
 import org.junit.After;
@@ -118,8 +119,90 @@ public class RegistryTest {
 
         //
         // Make sure that the method ran in the first object!
-        assertTrue(((RegistryConfigurableImpl) rc1).recs.size() == 1);
+        assertEquals(1, ((RegistryConfigurableImpl) rc1).recs.size());
         assertEquals(((RegistryConfigurableImpl) rc1).recs.get(0), "test");
+    }
+
+    @Test
+    public void testRegisterAndLookupAllWithEntries() throws IOException {
+
+        //
+        // Register in one manager.
+        cm1 = new JiniConfigurationManager("/com/oracle/labs/mlrg/olcut/config/remote/serverConfig.xml");
+        RegistryConfigurable rc1 = (RegistryConfigurable) cm1.lookup("servercompWithEntries");
+        RegistryConfigurable rc2 = (RegistryConfigurable) cm1.lookup("servercompWithEntriesA");
+        RegistryConfigurable rc3 = (RegistryConfigurable) cm1.lookup("servercompWithEntriesB");
+        RegistryConfigurable rc4 = (RegistryConfigurable) cm1.lookup("servercompWithEntriesC");
+        assertNotNull(rc1);
+        assertNotNull(rc2);
+        assertNotNull(rc3);
+        assertNotNull(rc4);
+
+        //
+        // LookupAll in another.
+        cm2 = new JiniConfigurationManager("/com/oracle/labs/mlrg/olcut/config/remote/clientConfig.xml");
+        ConfigurationEntries ce = (ConfigurationEntries) cm2.lookup("serverEntries");
+        List<RegistryConfigurable> rrc2 = (List<RegistryConfigurable>) cm2.lookupAll(RegistryConfigurable.class,null,ce.getEntries());
+
+        //
+        // Check we found all the elements
+        assertEquals(4,rrc2.size());
+
+        for (RegistryConfigurable r : rrc2) {
+            r.stringOp("test");
+        }
+
+        //
+        // Make sure that the methods ran in the server's objects
+        assertEquals(1, ((RegistryConfigurableImpl) rc1).recs.size());
+        assertEquals(((RegistryConfigurableImpl) rc1).recs.get(0), "test");
+        assertEquals(1, ((RegistryConfigurableImpl) rc2).recs.size());
+        assertEquals(((RegistryConfigurableImpl) rc2).recs.get(0), "test");
+        assertEquals(1, ((RegistryConfigurableImpl) rc3).recs.size());
+        assertEquals(((RegistryConfigurableImpl) rc3).recs.get(0), "test");
+        assertEquals(1, ((RegistryConfigurableImpl) rc4).recs.size());
+        assertEquals(((RegistryConfigurableImpl) rc4).recs.get(0), "test");
+    }
+
+    @Test
+    public void testRegisterAndLookupAllWithPartialMatchingEntries() throws IOException {
+
+        //
+        // Register in one manager.
+        cm1 = new JiniConfigurationManager("/com/oracle/labs/mlrg/olcut/config/remote/serverConfig.xml");
+        RegistryConfigurable rc1 = (RegistryConfigurable) cm1.lookup("servercompWithEntries");
+        RegistryConfigurable rc2 = (RegistryConfigurable) cm1.lookup("servercompWithEntriesA");
+        RegistryConfigurable rc3 = (RegistryConfigurable) cm1.lookup("servercompWithEntriesB");
+        RegistryConfigurable rc4 = (RegistryConfigurable) cm1.lookup("servercompWithEntriesC");
+        assertNotNull(rc1);
+        assertNotNull(rc2);
+        assertNotNull(rc3);
+        assertNotNull(rc4);
+
+        //
+        // LookupAll in another.
+        cm2 = new JiniConfigurationManager("/com/oracle/labs/mlrg/olcut/config/remote/clientConfig.xml");
+        ConfigurationEntries ce = (ConfigurationEntries) cm2.lookup("serverPartialMatchingEntries");
+        List<RegistryConfigurable> rrc2 = cm2.lookupAll(RegistryConfigurable.class,null,ce.getEntries());
+
+        //
+        // Check we found all the elements
+        assertEquals(4,rrc2.size());
+
+        for (RegistryConfigurable r : rrc2) {
+            r.stringOp("test");
+        }
+
+        //
+        // Make sure that the methods ran in the server's objects
+        assertEquals(1, ((RegistryConfigurableImpl) rc1).recs.size());
+        assertEquals(((RegistryConfigurableImpl) rc1).recs.get(0), "test");
+        assertEquals(1, ((RegistryConfigurableImpl) rc2).recs.size());
+        assertEquals(((RegistryConfigurableImpl) rc2).recs.get(0), "test");
+        assertEquals(1, ((RegistryConfigurableImpl) rc3).recs.size());
+        assertEquals(((RegistryConfigurableImpl) rc3).recs.get(0), "test");
+        assertEquals(1, ((RegistryConfigurableImpl) rc4).recs.size());
+        assertEquals(((RegistryConfigurableImpl) rc4).recs.get(0), "test");
     }
 
     @Test
