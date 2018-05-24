c373e16f8175ed71d2be5c201fc4aadeeb14cf9c
John Sullivan <john.t.sullivan@gmail.com>
2018-05-24 17:00:52 -0400

    added check that property names are not duplicated, along with tests

---
+++ olcut-config-edn/src/test/java/com/oracle/labs/mlrg/olcut/config/edn/DuplicateCompenentFieldTest.java
@@ -0,0 +1,33 @@
+package com.oracle.labs.mlrg.olcut.config.edn;
+
+import com.oracle.labs.mlrg.olcut.config.ConfigLoaderException;
+import com.oracle.labs.mlrg.olcut.config.ConfigurationManager;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DuplicateCompenentFieldTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        ConfigurationManager.addFileFormatFactory(new EdnConfigFactory());
+    }
+
+    @Test(expected = ConfigLoaderException.class)
+    public void duplicatePropertyMap() {
+        ConfigurationManager cm = new ConfigurationManager("duplicatePropertyMapConfig.edn");
+        Assert.fail("Should have thrown exception on loading");
+    }
+
+    @Test(expected = ConfigLoaderException.class)
+    public void duplicatePropertyList() {
+        ConfigurationManager cm = new ConfigurationManager("duplicatePropertyListConfig.edn");
+        Assert.fail("Should have thrown exception on loading");
+    }
+
+    @Test(expected = ConfigLoaderException.class)
+    public void duplicateProperty() {
+        ConfigurationManager cm = new ConfigurationManager("duplicatePropertyConfig.edn");
+        Assert.fail("Should have thrown exception on loading");
+    }
+}
