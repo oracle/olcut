28c174fb53a2c11ad1e05b8aeed7095524efe1c8
Adam Pocock <adam.pocock@oracle.com>
2019-04-02 17:47:33 -0400

    Refactoring DescribeConfigurable to make it easier to use from inside a REPL.

--- olcut-core/src/main/java/com/oracle/labs/mlrg/olcut/config/DescribeConfigurable.java
+++ olcut-core/src/main/java/com/oracle/labs/mlrg/olcut/config/DescribeConfigurable.java
@@ -3,6 +3,7 @@ package com.oracle.labs.mlrg.olcut.config;
 import com.oracle.labs.mlrg.olcut.config.xml.XMLConfigFactory;
 
 import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -118,7 +119,119 @@ public class DescribeConfigurable {
         }
     }
 
-    private static String formatDescription(List<List<String>> descriptions) {
+    public static TreeMap<String,FieldInfo> generateFieldInfo(Class<? extends Configurable> configurableClass) {
+        Set<Field> fieldSet = PropertySheet.getAllFields(configurableClass);
+
+        TreeMap<String, FieldInfo> map = new TreeMap<>();
+        for (Field f : fieldSet) {
+            //boolean accessible = f.isAccessible();
+            Config configAnnotation = f.getAnnotation(Config.class);
+            if (configAnnotation != null) {
+                FieldType ft = FieldType.getFieldType(f);
+                if (ft == null) {
+                    logger.warning("This class has an invalid configurable field type for field " + f.getName());
+                } else {
+                    logger.log(Level.FINEST, "Found field of type " + ft.name());
+
+                    //
+                    // Pull out the generic type from the list.
+                    if (FieldType.listTypes.contains(ft)) {
+                        List<Class<?>> genericList = PropertySheet.getGenericClass(f);
+                        if (genericList.size() == 1) {
+                            FieldInfo fi = new FieldInfo(f.getName(),f.getType().getName(),f,configAnnotation.mandatory(),configAnnotation.description(),genericList.get(0).getCanonicalName());
+                            map.put(f.getName(),fi);
+                        } else {
+                            logger.warning("This class has an invalid configurable field called " + f.getName() + ", failed to extract the generic type arguments for a list or set, found: " + genericList.toString());
+                        }
+                    } else if (FieldType.mapTypes.contains(ft)) {
+                        //
+                        // Pull out the generic types from the map.
+                        List<Class<?>> genericList = PropertySheet.getGenericClass(f);
+                        if (genericList.size() == 2) {
+                            FieldInfo fi = new FieldInfo(f.getName(),f.getType().getName(),f,configAnnotation.mandatory(),configAnnotation.description(),genericList.get(0).getCanonicalName(),genericList.get(1).getCanonicalName());
+                            map.put(f.getName(),fi);
+                        } else {
+                            logger.warning("This class has an invalid configurable field called " + f.getName() + ", failed to extract the generic type arguments for a map, found: " + genericList.toString());
+                        }
+                    } else {
+                        // Else write a standard FieldInfo record.
+                        FieldInfo fi = new FieldInfo(f.getName(),f.getType().getName(),f,configAnnotation.mandatory(),configAnnotation.description());
+                        map.put(f.getName(),fi);
+                    }
+                }
+            }
+        }
+
+        return map;
+    }
+
+    public static List<List<String>> generateDescription(Map<String,FieldInfo> map) {
+        List<List<String>> output = new ArrayList<>();
+
+        output.add(header);
+
+        for (Map.Entry<String, FieldInfo> e : map.entrySet()) {
+            ArrayList<String> fieldString = new ArrayList<>();
+
+            FieldInfo fi = e.getValue();
+
+            String type = fi.className;
+
+            switch (fi.type) {
+                case NORMAL:
+                    break;
+                case LIST:
+                    type = type + "<" + fi.genericListClass + ">";
+                    break;
+                case MAP:
+                    type = type + "<" + fi.genericMapKeyClass + "," + fi.genericMapValueClass + ">";
+                    break;
+            }
+
+            fieldString.add(fi.name);
+            fieldString.add(type);
+            fieldString.add(""+fi.mandatory);
+            fieldString.add(fi.description);
+
+            output.add(fieldString);
+        }
+
+        return output;
+    }
+
+    public static void writeExampleConfig(OutputStream stream, Class<? extends Configurable> configurableClass, Map<String,FieldInfo> map) {
+        XMLConfigFactory factory = new XMLConfigFactory();
+        ConfigWriter configWriter = factory.getWriter(stream);
+
+        // Generate attributes
+        Map<String,String> attributes = new HashMap<>();
+        attributes.put(ConfigLoader.NAME,"example");
+        attributes.put(ConfigLoader.EXPORT,"false");
+        attributes.put(ConfigLoader.IMPORT,"false");
+        attributes.put(ConfigLoader.TYPE,configurableClass.getCanonicalName());
+
+        // Generate default properties
+        Map<String,Object> properties = new HashMap<>();
+        for (Map.Entry<String,FieldInfo> e : map.entrySet()) {
+            FieldInfo fi = e.getValue();
+            switch (fi.type) {
+                case NORMAL:
+                    properties.put(e.getKey(),generateDefaultValue(fi));
+                    break;
+                case LIST:
+                    properties.put(e.getKey(),Arrays.asList(fi.className + "-instance"));
+                    break;
+                case MAP:
+                    Map<String,String> newMap = new HashMap<>();
+                    newMap.put("mapKey",fi.genericMapValueClass+"-instance");
+                    properties.put(e.getKey(),newMap);
+                    break;
+            }
+        }
+        configWriter.writeComponent(attributes, properties);
+    }
+
+    public static String formatDescription(List<List<String>> descriptions) {
         int[] maxWidth = new int[4];
 
         for (List<String> a : descriptions) {
@@ -173,77 +286,10 @@ public class DescribeConfigurable {
             Class<?> clazz = Class.forName(o.className);
             if (Configurable.class.isAssignableFrom(clazz)) {
                 Class<? extends Configurable> configurableClass = (Class<? extends Configurable>) clazz;
-                Set<Field> fieldSet = PropertySheet.getAllFields(configurableClass);
 
-                TreeMap<String, FieldInfo> map = new TreeMap<>();
-                for (Field f : fieldSet) {
-                    //boolean accessible = f.isAccessible();
-                    Config configAnnotation = f.getAnnotation(Config.class);
-                    if (configAnnotation != null) {
-                        FieldType ft = FieldType.getFieldType(f);
-                        if (ft == null) {
-                            logger.warning("This class has an invalid configurable field type for field " + f.getName());
-                        } else {
-                            logger.log(Level.FINEST, "Found field of type " + ft.name());
+                Map<String,FieldInfo> map = generateFieldInfo(configurableClass);
 
-                            //
-                            // Pull out the generic type from the list.
-                            if (FieldType.listTypes.contains(ft)) {
-                                List<Class<?>> genericList = PropertySheet.getGenericClass(f);
-                                if (genericList.size() == 1) {
-                                    FieldInfo fi = new FieldInfo(f.getName(),f.getType().getName(),f,configAnnotation.mandatory(),configAnnotation.description(),genericList.get(0).getCanonicalName());
-                                    map.put(f.getName(),fi);
-                                } else {
-                                    logger.warning("This class has an invalid configurable field called " + f.getName() + ", failed to extract the generic type arguments for a list or set, found: " + genericList.toString());
-                                }
-                            } else if (FieldType.mapTypes.contains(ft)) {
-                                //
-                                // Pull out the generic types from the map.
-                                List<Class<?>> genericList = PropertySheet.getGenericClass(f);
-                                if (genericList.size() == 2) {
-                                    FieldInfo fi = new FieldInfo(f.getName(),f.getType().getName(),f,configAnnotation.mandatory(),configAnnotation.description(),genericList.get(0).getCanonicalName(),genericList.get(1).getCanonicalName());
-                                    map.put(f.getName(),fi);
-                                } else {
-                                    logger.warning("This class has an invalid configurable field called " + f.getName() + ", failed to extract the generic type arguments for a map, found: " + genericList.toString());
-                                }
-                            } else {
-                                // Else write a standard FieldInfo record.
-                                FieldInfo fi = new FieldInfo(f.getName(),f.getType().getName(),f,configAnnotation.mandatory(),configAnnotation.description());
-                                map.put(f.getName(),fi);
-                            }
-                        }
-                    }
-                }
-
-                List<List<String>> output = new ArrayList<>();
-
-                output.add(header);
-
-                for (Map.Entry<String, FieldInfo> e : map.entrySet()) {
-                    ArrayList<String> fieldString = new ArrayList<>();
-
-                    FieldInfo fi = e.getValue();
-
-                    String type = fi.className;
-
-                    switch (fi.type) {
-                        case NORMAL:
-                            break;
-                        case LIST:
-                            type = type + "<" + fi.genericListClass + ">";
-                            break;
-                        case MAP:
-                            type = type + "<" + fi.genericMapKeyClass + "," + fi.genericMapValueClass + ">";
-                            break;
-                    }
-
-                    fieldString.add(fi.name);
-                    fieldString.add(type);
-                    fieldString.add(""+fi.mandatory);
-                    fieldString.add(fi.description);
-
-                    output.add(fieldString);
-                }
+                List<List<String>> output = generateDescription(map);
 
                 System.out.println("Class: " + configurableClass.getCanonicalName() + "\n");
                 System.out.println(formatDescription(output));
@@ -251,35 +297,7 @@ public class DescribeConfigurable {
                 if (o.output) {
                     ByteArrayOutputStream writer = new ByteArrayOutputStream();
 
-                    XMLConfigFactory factory = new XMLConfigFactory();
-                    ConfigWriter configWriter = factory.getWriter(writer);
-
-                    // Generate attributes
-                    Map<String,String> attributes = new HashMap<>();
-                    attributes.put(ConfigLoader.NAME,"example");
-                    attributes.put(ConfigLoader.EXPORT,"false");
-                    attributes.put(ConfigLoader.IMPORT,"false");
-                    attributes.put(ConfigLoader.TYPE,configurableClass.getCanonicalName());
-
-                    // Generate default properties
-                    Map<String,Object> properties = new HashMap<>();
-                    for (Map.Entry<String,FieldInfo> e : map.entrySet()) {
-                        FieldInfo fi = e.getValue();
-                        switch (fi.type) {
-                            case NORMAL:
-                                properties.put(e.getKey(),generateDefaultValue(fi));
-                                break;
-                            case LIST:
-                                properties.put(e.getKey(),Arrays.asList(fi.className + "-instance"));
-                                break;
-                            case MAP:
-                                Map<String,String> newMap = new HashMap<>();
-                                newMap.put("mapKey",fi.genericMapValueClass+"-instance");
-                                properties.put(e.getKey(),newMap);
-                                break;
-                        }
-                    }
-                    configWriter.writeComponent(attributes, properties);
+                    writeExampleConfig(writer,configurableClass,map);
 
                     System.out.println("Example :\n" + writer.toString("UTF-8"));
                 }
