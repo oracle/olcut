8ba919b798a0158a56243f8df7c7099385ff8fc2
Philip Ogren <philip.ogren@oracle.com>
2019-04-08 10:08:21 -0600

    move cm.close() to end of test

--- olcut-core/src/test/java/com/oracle/labs/mlrg/olcut/config/TypedProcessorsTest.java
+++ olcut-core/src/test/java/com/oracle/labs/mlrg/olcut/config/TypedProcessorsTest.java
@@ -11,7 +11,6 @@ public class TypedProcessorsTest {
 	@Test
 	public void testStringType() throws Exception {
 		ConfigurationManager cm = new ConfigurationManager("/com/oracle/labs/mlrg/olcut/config/typedProcessorsConfig.xml");
-		cm.close();
 		@SuppressWarnings("unchecked")
 		TypedProcessorList<String, String> tpl = (TypedProcessorList<String, String>) cm.lookup("typedProcessors");
 		List<String> values = tpl.process("Omelet");
@@ -19,5 +18,6 @@ public class TypedProcessorsTest {
 
 		assertEquals("McOmeletFace", values.get(0));
 		assertEquals("telemO-1170105035", values.get(1));
+		cm.close();
 	}
 }
