33a66838ae29a45054b6d402f576f3446cb3d4f1
Adam Pocock <adam.pocock@oracle.com>
2019-07-16 22:27:32 -0400

    Making MutableNumber.copyMap properly generic.

--- olcut-core/src/main/java/com/oracle/labs/mlrg/olcut/util/MutableNumber.java
+++ olcut-core/src/main/java/com/oracle/labs/mlrg/olcut/util/MutableNumber.java
@@ -7,12 +7,12 @@ public abstract class MutableNumber extends Number {
 
     public abstract MutableNumber copy();
 
-    public static <T extends MutableNumber> Map<String,T> copyMap(Map<String,T> input) {
-        HashMap<String, T> output = new HashMap<>();
+    public static <T,U extends MutableNumber> Map<T,U> copyMap(Map<T,U> input) {
+        HashMap<T,U> output = new HashMap<>();
 
-        for (Map.Entry<String, T> e : input.entrySet()) {
+        for (Map.Entry<T,U> e : input.entrySet()) {
             @SuppressWarnings("unchecked") //copy returns the same type.
-            T copy = (T) e.getValue().copy();
+            U copy = (U) e.getValue().copy();
             output.put(e.getKey(), copy);
         }
 
