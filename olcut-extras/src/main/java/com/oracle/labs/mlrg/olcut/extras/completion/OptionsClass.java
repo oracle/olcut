171927636994132617cbdec916046984868d4d09
John Sullivan <jack.t.sullivan@oracle.com>
2020-04-15 17:40:08 -0400

    fixed issue where default options were appearing multiple times.

--- olcut-extras/src/main/java/com/oracle/labs/mlrg/olcut/extras/completion/OptionsClass.java
+++ olcut-extras/src/main/java/com/oracle/labs/mlrg/olcut/extras/completion/OptionsClass.java
@@ -24,15 +24,16 @@ public class OptionsClass {
         this.localCompletions = new ArrayList<>();
         this.parents = new ArrayList<>();
         this.allCompletions = new ArrayList<>();
-        defaultCompletions(this.allCompletions);
     }
 
     // There are some completions that come by default with a configuration manager that we should also support
-    private void defaultCompletions(List<OptionCompletion> comps) {
+    private List<OptionCompletion> defaultCompletions() {
         // TODO It would probably be better to get these directly from ConfigurationManager, but this we can avoid depending on olcut-core
+        List<OptionCompletion> comps = new ArrayList<>();
         comps.add(new OptionCompletion('c', "config-file", "A comma separated list of olcut config files."));
         comps.add(new OptionCompletion("config-file-formats", "A comma separated list of olcut FileFormatFactory implementations (assumed to be on the classpath)."));
         comps.add(new OptionCompletion("usage" , "Write out this usage/help statement."));
+        return comps;
     }
 
     public void addCompletion(OptionCompletion completion) {
@@ -40,10 +41,17 @@ public class OptionsClass {
     }
 
     public List<OptionCompletion> getCompletions() {
+        List<OptionCompletion> comps = innerGetCompletions();
+        // TODO there is some nonsense on Configuration manager that can turn of -c and --config-file-formats, not currently handled
+        comps.addAll(defaultCompletions());
+        return comps;
+    }
+
+    private List<OptionCompletion> innerGetCompletions() {
         if(!cachedCompletions) {
             allCompletions.addAll(localCompletions);
             allCompletions.addAll(parents.stream()
-                    .flatMap(p -> p.getCompletions().stream())
+                    .flatMap(p -> p.innerGetCompletions().stream())
                     .collect(Collectors.toList()));
             cachedCompletions = true;
         }
